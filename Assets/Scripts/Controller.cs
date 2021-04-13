7a8e737a0a53b139743f845c8e6e4da72646671e
orvilline <thunhi.h@gmail.com>
2021-04-13 09:14:41 +0200

    C# added

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{   public void ChangeScene()
+    {
+        SceneManager.LoadScene("Screen2");
+    }
+}
